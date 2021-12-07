8f470d4aea014d258f42815bb337fc5e6668aa3a
yoichi <itiyou14@gmail.com>
2021-12-07 16:05:00 +0900

    ログイン後のリダイレクト先を設定、ログイン、アカウント登録のリンクを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    test_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    test_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    root_path
+  end
+
 end
