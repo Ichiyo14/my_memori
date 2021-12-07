8546ada9680f880cf5e8b144fed5e9d03f6019dc
yoichi <itiyou14@gmail.com>
2021-12-07 16:46:20 +0900

    userモデルのテストを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource)
     root_path
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with email and password' do
+    user = described_class.new(
+      email: 'abc@example.com',
+      password: 'testtest'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without an email address' do
+    user = described_class.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include('を入力してください')
+  end
+
+  it 'is invalid with a duplicate email address' do
+    described_class.create(
+      email: 'abc@example.com',
+      password: 'testtest'
+    )
+    user = described_class.new(
+      email: 'abc@example.com',
+      password: 'testtest'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('はすでに存在します')
+  end
 end
