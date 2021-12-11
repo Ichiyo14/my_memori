7a3389cfedf3c489677fc46ae689e5c245e8d2f8
yoichi <itiyou14@gmail.com>
2021-12-11 15:34:40 +0900

    lintを通した

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Question < ApplicationRecord
   belongs_to :questionnaire
   belongs_to :factor
-  has_many :scales
+  has_many :scales, dependent: :destroy
   has_many :answer, dependent: :destroy
 end
--- app/models/scale.rb
+++ app/models/scale.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Scale < ApplicationRecord
   belongs_to :question
   has_many :answers, dependent: :destroy
