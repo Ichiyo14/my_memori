f2a32d6eb885d3b57fa2a6c5414e5245965a00cb
yoichi <itiyou14@gmail.com>
2021-12-15 09:11:27 +0900

    vue.jsをインストールした

---
+++ app/javascript/hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hello: "Helloaaa",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
