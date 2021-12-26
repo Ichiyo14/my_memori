9162f01adeca3493ebdc29c2a343d2a4cb18573c
yoichi <itiyou14@gmail.com>
2021-12-26 09:18:25 +0900

    bulmaをインストールし、testファイルへ適用

--- app/javascript/hello.vue
+++ app/javascript/hello.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 p {{ hello }}
 div
-  h1 'hoge'
+  h2.title.is-2 'hoge'
 </template>
 
 <script>
 export default {
   data() {
     return {
-      hello: "Helloaaa",
+      hello: "Hello",
     };
   },
 };
